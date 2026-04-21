4b75ea2713a020c9f6100cec3af8b415c25c07a8
Dennisss02 <denis_aandrei02@yahoo.com>
2026-04-21 10:26:40 +0300

    Lab8: exercitiile 1 si 2

--- src/com/pao/laboratory08/exercise1/Main.java
+++ src/com/pao/laboratory08/exercise1/Main.java
@@ -17,6 +17,60 @@ public class Main {
         //    - SHALLOW <nume> → shallow clone + modifică orașul clonei la "MODIFICAT" + afișează
         //    - DEEP <nume> → deep clone + modifică orașul clonei la "MODIFICAT" + afișează
 
-        System.out.println("TODO: implementează exercițiul 1");
+        BufferedReader fin = new BufferedReader(new FileReader(FILE_PATH));
+
+        ArrayList<Student> studenti = new ArrayList<>();
+        String linie;
+        while ((linie = fin.readLine()) != null) {
+            if(linie.trim().isEmpty()) {
+                continue;
+            }
+
+            String[] param = linie.split(",");
+            String nume = param[0].trim();
+            int varsta = Integer.parseInt(param[1].trim());
+            String oras = param[2].trim();
+            String strada = param[3].trim();
+            Adresa adresa = new Adresa(oras, strada);
+            studenti.add(new Student(nume, varsta, adresa));
+        }
+        fin.close();
+
+        Scanner scanner = new Scanner(System.in);
+        String[] comanda = scanner.nextLine().split(" ");
+        if(comanda[0].equals("PRINT")) {
+            for(Student student: studenti) {
+                System.out.println(student);
+            }
+        }
+        else if(comanda[0].equals("SHALLOW")) {
+            Student studentGasit = studenti.stream()
+                    .filter(s -> s.getNume().equals(comanda[1]))
+                    .findFirst()
+                    .orElse(null);
+            if (studentGasit == null) {
+                System.out.println("Studentul nu a fost gasit.");
+            } else {
+                Student clona = studentGasit.shallowClone();
+                clona.setOras("MODIFICAT");
+                System.out.println("Original: " + studentGasit);
+                System.out.println("Clona: " + clona);
+            }
+        }
+        else if(comanda[0].equals("DEEP")) {
+            Student studentGasit = studenti.stream()
+                    .filter(s -> s.getNume().equals(comanda[1]))
+                    .findFirst()
+                    .orElse(null);
+            if (studentGasit == null) {
+                System.out.println("Studentul nu a fost gasit.");
+            } else {
+                Student clona = studentGasit.deepClone();
+                clona.setOras("MODIFICAT");
+                System.out.println("Original: " + studentGasit);
+                System.out.println("Clona: " + clona);
+            }
+        }
+        scanner.close();
     }
 }
--- src/com/pao/laboratory08/exercise2/Main.java
+++ src/com/pao/laboratory08/exercise2/Main.java
@@ -1,10 +1,14 @@
 package com.pao.laboratory08.exercise2;
 
+import com.pao.laboratory08.exercise1.Adresa;
+import com.pao.laboratory08.exercise1.Student;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static final String FILE_PATH = "src/com/pao/laboratory08/tests/studenti.txt";
+    private static final String INPUT_PATH = "src/com/pao/laboratory08/tests/studenti.txt";
+    private static final String OUTPUT_PATH = "src/com/pao/laboratory08/exercise2/rezultate.txt";
 
     public static void main(String[] args) throws Exception {
         // TODO: Implementează conform Readme.md
@@ -15,7 +19,45 @@ public class Main {
         // 4. Scrie filtrații în "rezultate.txt" cu BufferedWriter
         // 5. Afișează sumarul la consolă
 
-        System.out.println("TODO: implementează exercițiul 2");
+        BufferedReader fin = new BufferedReader(new FileReader(INPUT_PATH));
+        BufferedWriter fout = new BufferedWriter(new FileWriter(OUTPUT_PATH));
+
+        ArrayList<Student> studenti = new ArrayList<>();
+        String linie;
+        while ((linie = fin.readLine()) != null) {
+            if(linie.trim().isEmpty()) {
+                continue;
+            }
+
+            String[] param = linie.split(",");
+            String nume = param[0].trim();
+            int varsta = Integer.parseInt(param[1].trim());
+            String oras = param[2].trim();
+            String strada = param[3].trim();
+            Adresa adresa = new Adresa(oras, strada);
+            studenti.add(new Student(nume, varsta, adresa));
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int prag = scanner.nextInt();
+        List<Student> rezultat = studenti.stream()
+                        .filter(s -> s.getVarsta() >= prag)
+                        .toList();
+
+        for(Student s: rezultat) {
+            fout.write(s.toString());
+            fout.newLine();
+        }
+        System.out.println("Filtru: varsta >= " + prag);
+        System.out.println("Rezultate: " + rezultat.size() + " studenti\n");
+        for(Student s: rezultat) {
+            System.out.println(s);
+        }
+        System.out.println("\nScris in: rezultate.txt");
+
+
+        fin.close();
+        fout.close();
     }
 }
 
